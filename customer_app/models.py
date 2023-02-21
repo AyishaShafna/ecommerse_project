e622f46e8dc0441e85999c2e659499371ca6b874
AyishaShafna <ayishashafna123@gmail.com>
2023-02-21 09:30:16 -0800

    first push

--- customer_app/models.py
+++ customer_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from seller.models import Products
+from common_app.models Customer
+
 
 # Create your models here.
+class Cart(models.Model):
+    product = models.ForeignKey(Products,on_delete=models.CASCADE,on_update=models.CASCADE)
+    customer = models.ForeignKey(Customer,on_delete=models.CASCADE,on_update=models.CASCADE)
